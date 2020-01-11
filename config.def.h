0630524b6ae22b5fab8264bd0d3b53f0436e03bd
JhonRocha <jhmrocha@gmail.com>
2020-01-11 09:56:29 -0300

    Fixing fonts

--- config.def.h
+++ config.def.h
@@ -8,8 +8,9 @@
 static char *font = "UbuntuMono Nerd Font:pixelsize=18:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-/*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
-/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
+  "Siji:pixelsize=18",
+	"Inconsolata for Powerline:pixelsize=18:antialias=true:autohint=true",
+	"Hack Nerd Font Mono:pixelsize=18:antialias=true:autohint=true",
 };
 
 static int borderpx = 2;
